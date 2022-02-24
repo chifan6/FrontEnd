ed6c386538fd4d185cf187a64dc2b5cd01e23f1e
Flag <1649823510@qq.com>
2022-02-24 14:23:46 +0800

    模拟axios发送请求和添加一些开源文件

--- axios/src/js/T_01.js
+++ axios/src/js/T_01.js
@@ -6,7 +6,10 @@ function Axios(config){
     };
 }
 Axios.prototype.request = function (config){
-    console.log(`发送AJAX请求 ${config.method}`)
+    let promise = Promise.resolve(config);
+    let chains = [dispatchRequest, undefined];
+    let result = promise.then(chains[0], chains[1]);
+    return result;
 }
 Axios.prototype.get = function (config){
     return this.request(config)
@@ -34,6 +37,45 @@ function createInstance(config){
     })
     return instance;
 }
+function dispatchRequest(config){
+    return xhrAdapter(config).then(response=>{
+        return response
+    },error=>{
+        throw error
+    })
+}
+function xhrAdapter(config){
+    return new Promise(((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(config.method,config.url);
+        xhr.send();
+        xhr.onreadystatechange = function (){
+            if(xhr.readyState === 4){
+                if (xhr.status >= 200 && xhr.status < 300){
+                    resolve({
+                        config:config,
+                        data:xhr.response,
+                        headers:xhr.getAllResponseHeaders(),
+                        request:xhr,
+                        status:xhr.status,
+                        statusText: xhr.statusText,
+                    });
+                }else {
+                    reject(new Error(`请求失败 失败的状态码为${xhr.status}`))
+                }
+            }
+        }
+    }))
+}
 
 let axios = createInstance()
-axios.get({method:"get"})
+axios({
+    method: "GET",
+    url: "http://localhost:3000/posts/1",
+    /*cancelToken: new axios.CancelToken(function (cancel) {
+        //执行了cancel则会关闭这个Ajax请求
+        cancelAxios = cancel;
+    })*/
+}).then(Response => {
+    console.log(Response)
+});
