88e4ec089398e00c7c18f0baeae9fd987acb3161
Flag <1649823510@qq.com>
2022-03-19 00:56:07 +0800

    github搜索 静态模板

--- React/react-staging/src/App.jsx
+++ React/react-staging/src/App.jsx
@@ -1,34 +1,13 @@
 import React, { Component } from "react";
-import axios from "axios";
+import List from "./component/List";
+import Section from "./component/Section";
 
 export default class App extends Component {
-  //处理获取学生的回调
-  getStudents = () => {
-    axios.get("http://localhost:3000/api1/students").then(
-      (respone) => {
-        console.log(respone.data);
-      },
-      (renson) => {
-        throw renson;
-      }
-    );
-  };
-  //处理获取汽车的回调
-  getCars = () => {
-    axios.get("http://localhost:3000/api2/cars").then(
-      (respone) => {
-        console.log(respone.data);
-      },
-      (renson) => {
-        throw renson;
-      }
-    );
-  };
   render() {
     return (
-      <div>
-        <button onClick={this.getStudents}>点击获取学生信息</button>
-        <button onClick={this.getCars}>点击获取汽车信息</button>
+      <div className="container">
+        <Section />
+        <List />
       </div>
     );
   }
