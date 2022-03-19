4f24caee05d557005b6621417f28c12fd46ead0b
Flag <1649823510@qq.com>
2022-03-19 16:28:28 +0800

    github搜索

--- React/react-staging/src/App.jsx
+++ React/react-staging/src/App.jsx
@@ -3,11 +3,18 @@ import List from "./component/List";
 import Section from "./component/Section";
 
 export default class App extends Component {
+  //初始化状态
+  //用户信息，是否是第一次加载，是否加载中，报错信息
+  state = { users: [], isFirst: true, isLoading: false, err: "" };
+  //保存用户数据到状态的回调
+  seveUsers = (usersObj) => {
+    this.setState(usersObj);
+  };
   render() {
     return (
       <div className="container">
-        <Section />
-        <List />
+        <Section seveUsers={this.seveUsers} />
+        <List {...this.state} />
       </div>
     );
   }
--- React/react-staging/src/component/List/index.jsx
+++ React/react-staging/src/component/List/index.jsx
@@ -3,58 +3,32 @@ import "./index.css";
 
 export default class index extends Component {
   render() {
+    const { users, isFirst, isLoading, err } = this.props;
+    console.log();
     return (
       <div className="row">
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img
-              alt="head_portrait"
-              src="https://avatars.githubusercontent.com/u/6412038?v=3"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img
-              alt="head_portrait"
-              src="https://avatars.githubusercontent.com/u/6412038?v=3"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img
-              alt="head_portrait"
-              src="https://avatars.githubusercontent.com/u/6412038?v=3"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img
-              alt="head_portrait"
-              src="https://avatars.githubusercontent.com/u/6412038?v=3"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img
-              alt="head_portrait"
-              src="https://avatars.githubusercontent.com/u/6412038?v=3"
-              style={{ width: "100px" }}
-            />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
+        {isFirst ? (
+          <h2 style={{ margin: "0 515px" }}>welcome!</h2>
+        ) : isLoading ? (
+          <h2 style={{ margin: "0 515px" }}>Loading...</h2>
+        ) : err ? (
+          <div style={{ margin: "0 441px", color: "red" }}>{err.message}</div>
+        ) : (
+          users.map((user) => {
+            return (
+              <div key={user.id} className="card">
+                <a rel="noreferrer" href={user.html_url} target="_blank">
+                  <img
+                    alt="head_portrait"
+                    src={user.avatar_url}
+                    style={{ width: "100px" }}
+                  />
+                </a>
+                <p className="card-text">{user.login}</p>
+              </div>
+            );
+          })
+        )}
       </div>
     );
   }
