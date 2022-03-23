19a587513ea9b06683f2866cef3da07e4b6c037e
Flag <1649823510@qq.com>
2022-03-24 01:46:16 +0800

    antd使用与配置

--- React/react-staging/src/App.jsx
+++ React/react-staging/src/App.jsx
@@ -1,52 +1,20 @@
-import React, { Component } from "react";
-import { NavLink, Route, Switch, Link, Redirect } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import MyNavLink from "./component/MyNavLink";
-import Test from "./pages/Test";
-import Header from "./component/Header";
+import React from "react";
+import { Button } from "antd";
+import "antd/dist/antd.less";
+import { StepBackwardOutlined, LeftCircleTwoTone } from "@ant-design/icons";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-xs-8">
-            <Header />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              {/* NavLink 默认有添加active类的属性 如果要添加的属性也叫active可以省略 */}
-              {/* MyNavLink 自定义组件  可以根据自己的需求添加属性 
-              下面的About文本属性也会被props收集使用children特殊的属性可以拿到当前的文本内容 */}
-              <MyNavLink to="/about">About</MyNavLink>
-              <NavLink className="list-group-item" to="/home">
-                Home
-              </NavLink>
-              <Link className="list-group-item" to="/test">
-                Test
-              </Link>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                {/* 包裹再Switch里面的Route只要匹配到了一个就会停止向下匹配以提高效率  */}
-                <Switch>
-                  <Route path={"/about"} component={About} />
-                  <Route path={"/home"} component={Home} />
-                  {/* exact 开启严格匹配  Link需要和Route的路径要完美匹配才能实现效果 */}
-                  <Route path={"/test"} component={Test} />
-                  {/* Redirect组件 需要放在Route组件的后面 当上面的Route组件都没有匹配到地址时Redirect组件可以指定到一个路径 */}
-                  <Redirect to={"/about"} />
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const test = () => {
+  console.log("test");
+};
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+    <StepBackwardOutlined
+      style={{ fontSize: "129px", color: "red" }}
+      onClick={test}
+    />
+    <LeftCircleTwoTone spin={true} />
+  </div>
+);
+
+export default App;
--- React/react-staging/src/index.jsx
+++ React/react-staging/src/index.jsx
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(
-  // BorwserRoute 1、对state刷新时不会丢失数据 2、不兼容IE9及以下的版本
-  // HashRouter   1、对state刷新的数据会丢失 2、兼容性比BorwserRoute高
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
