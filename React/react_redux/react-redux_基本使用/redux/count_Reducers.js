427ffbb666f89f4146223affc59c7bc74eae3feb
Flag <1649823510@qq.com>
2022-03-29 20:52:30 +0800

    react-redux基本使用

---
+++ React/react_redux/react-redux_基本使用/redux/count_Reducers.js
@@ -0,0 +1,15 @@
+// 引入type防止写错
+import { PLUS, MINUS } from "./constate";
+
+export default function countReducer(preState, action) {
+  const { type, date } = action;
+  switch (type) {
+    case PLUS:
+      return preState + date;
+    case MINUS:
+      return preState - date;
+    default:
+      //设置初始化
+      return 0;
+  }
+}
