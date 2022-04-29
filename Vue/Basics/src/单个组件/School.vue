120a41826c13d595ffa32aabcaa2f8f092afbcd7
Flag <1649823510@qq.com>
2022-04-29 16:35:05 +0800

    Vue脚手架

---
+++ Vue/Basics/src/单个组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 class="demo">{{name}}</h1>
+    <button @click="showName">点我显示学校名称</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"School",
+  data(){
+    return {
+      name:"glnc"
+    }
+  },
+  methods:{
+    showName(){
+      console.log(this.name)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  demo{
+    width: 200px;
+    height: 200px;
+    background-color: pink;
+  }
+</style>
---
+++ Vue/Basics/src/单个组件/Student.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{name}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Student",
+  data(){
+    return {
+      name:"jock"
+    }
+  }
+}
+</script>
