eda9394ef01a1e8007c52acc9245112ff37e3d76
Flag <1649823510@qq.com>
2022-05-17 00:03:33 +0800

    插槽

---
+++ Vue/Staging/D10_github案例/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="container">
+      <Search/>
+      <List/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search";
+import List from "./components/List";
+export default {
+  name: "App",
+  components: {
+    Search,
+    List
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ Vue/Staging/src/components/Category.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <h3 class="title">{{ title }}</h3>
+    <!--    默认插槽-->
+    <slot></slot>
+    <!--  具名插槽  -->
+    <slot name="films"></slot>
+    <!--作用域插槽-->
+    <!--向插槽的使用者（父组件）传递参数-->
+    <slot name="tests" :tests="tests" :test2="tests2"></slot>
+
+    <!--插槽默认配置-->
+    <slot name="video">
+      <h6>如果没又使用到该插槽则会使用默认的配置</h6>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ["title"],
+  data() {
+    return {
+      tests: ["test1", "test2", "test3", "test4"],
+      tests2: ["test5"]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  width: 200px;
+  height: 300px;
+  background-color: skyblue;
+  float: left;
+  margin-left: 20px;
+}
+
+.title {
+  background-color: orange;
+  margin-top: 20px;
+  text-align: center;
+  line-height: 40px;
+}
+</style>
