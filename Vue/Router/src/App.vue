84bd741b7ea0e645ae5e807041bac494884a0cbe
Flag <1649823510@qq.com>
2022-05-31 23:05:11 +0800

    路由基本使用

--- Vue/Router/src/App.vue
+++ Vue/Router/src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <div class="row">
+        <div class="col-xs-offset-2 col-xs-8">
+          <div class="page-header"><h2>Vue Router Demo</h2></div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-2 col-xs-offset-2">
+          <div class="list-group">
+            <router-link class="list-group-item" active-class="active" to="/about">About</router-link>
+            <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
+          </div>
+        </div>
+        <div class="col-xs-6">
+          <div class="panel">
+            <div class="panel-body">
+              <router-view></router-view>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  color: #003366;
+}
+
+button {
+  border: #5bc0de 1px solid;
+  background-color: #00b3ee;
+}
+
+button:hover {
+  border: #5bc0de 1px solid;
+  background-color: #00aa00;
+}
+
+body {
+  background-repeat: no-repeat;
+  background-size: 1920px 960px;
+  background-image: linear-gradient(15deg, #13547a 0%, #80d0c7 100%);
 }
 </style>
